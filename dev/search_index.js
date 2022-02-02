591be99918ed52f1c0b53c1ffdb50165a90c95c2
Documenter.jl <documenter@juliadocs.github.io>
2022-02-02 16:32:18 +0000

    build based on ae7f199

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FunctionWrappersWrappers","category":"page"},{"location":"#FunctionWrappersWrappers","page":"Home","title":"FunctionWrappersWrappers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FunctionWrappersWrappers.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FunctionWrappersWrappers]","category":"page"}]
+}
