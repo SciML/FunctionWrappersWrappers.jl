ae7f1991072fafaec0d99dbcb2ce0ce417c6bc22
Chris Elrod <elrodc@gmail.com>
2022-02-02 11:28:01 -0500

    create repo

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://chriselrod.github.io/FunctionWrappersWrappers.jl/dev)
 [![Build Status](https://github.com/chriselrod/FunctionWrappersWrappers.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/chriselrod/FunctionWrappersWrappers.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/chriselrod/FunctionWrappersWrappers.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/chriselrod/FunctionWrappersWrappers.jl)
+
+
+If wrapping your functions once makes them better, why not try wrapping them twice?
+
--- src/FunctionWrappersWrappers.jl
+++ src/FunctionWrappersWrappers.jl
@@ -1,5 +1,23 @@
 module FunctionWrappersWrappers
 
-# Write your package code here.
+using FunctionWrappers
+
+export FunctionWrappersWrapper
+
+struct FunctionWrappersWrapper{FW}
+  fw::FW
+end
+(fww::FunctionWrappersWrapper{FW})(args::Vararg{Any,K}) where {FW,K} = _call(fww.fw, args)
+
+_call(fw::Tuple{FunctionWrappers.FunctionWrapper{R,A},Vararg}, arg::A) where {R,A} = first(fw)(arg...)
+_call(fw::Tuple{FunctionWrappers.FunctionWrapper{R,A1},Vararg}, arg::A2) where {R,A1,A2} = _call(Base.tail(fw), arg)
+_call(::Tuple{}, arg) = throw("No matching function wrapper was found!")
+
+function FunctionWrappersWrapper(f::F, argtypes::Tuple{Vararg{Any,K}}, rettypes::Tuple{Vararg{DataType,K}}) where {F,K}
+  fwt = map(argtypes, rettypes) do A, R
+    FunctionWrappers.FunctionWrapper{R,A}(f)
+  end
+  FunctionWrappersWrapper(fwt)
+end
 
 end
