995c73d34b172a94aad6f30d5471e4a3c19ed386
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-08-24 06:30:52 -0400

    Add a fallback mode

--- src/FunctionWrappersWrappers.jl
+++ src/FunctionWrappersWrappers.jl
@@ -4,21 +4,22 @@ using FunctionWrappers
 
 export FunctionWrappersWrapper
 
-struct FunctionWrappersWrapper{FW}
+struct FunctionWrappersWrapper{FW,FB}
   fw::FW
   original::Any
 end
-(fww::FunctionWrappersWrapper{FW})(args::Vararg{Any,K}) where {FW,K} = _call(fww.fw, args)
+(fww::FunctionWrappersWrapper{FW,FB})(args::Vararg{Any,K}) where {FW,K,FB} = _call(fww.fw, args, Val{FB})
 
-_call(fw::Tuple{FunctionWrappers.FunctionWrapper{R,A},Vararg}, arg::A) where {R,A} = first(fw)(arg...)
-_call(fw::Tuple{FunctionWrappers.FunctionWrapper{R,A1},Vararg}, arg::A2) where {R,A1,A2} = _call(Base.tail(fw), arg)
-_call(::Tuple{}, arg) = throw("No matching function wrapper was found!")
+_call(fw::Tuple{FunctionWrappers.FunctionWrapper{R,A},Vararg}, arg::A, fww::FunctionWrappersWrapper{FW,FB}) where {R,A,FW,FB} = first(fw)(arg...)
+_call(fw::Tuple{FunctionWrappers.FunctionWrapper{R,A1},Vararg}, arg::A2, fww::FunctionWrappersWrapper{FW,FB}) where {R,A1,A2,FW,FB} = _call(Base.tail(fw), arg, fww)
+_call(::Tuple{}, arg, fww::FunctionWrappersWrapper{FW,false}) = throw("No matching function wrapper was found!")
+_call(::Tuple{}, arg, fww::FunctionWrappersWrapper{FW,true}) = fw.original(arg...)
 
-function FunctionWrappersWrapper(f::F, argtypes::Tuple{Vararg{Any,K}}, rettypes::Tuple{Vararg{DataType,K}}) where {F,K}
+function FunctionWrappersWrapper(f::F, argtypes::Tuple{Vararg{Any,K}}, rettypes::Tuple{Vararg{DataType,K}}, fallback::Val{FB}=Val{false}) where {F,K,FB}
   fwt = map(argtypes, rettypes) do A, R
     FunctionWrappers.FunctionWrapper{R,A}(f)
   end
-  FunctionWrappersWrapper(fwt,f)
+  FunctionWrappersWrapper{typeof(fwt),FB}(fwt,f)
 end
 
 end
